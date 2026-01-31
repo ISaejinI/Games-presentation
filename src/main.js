0900e0b7e1e64abba5ef3a4458ee65a172a38751
Saejin <saejinmx@gmail.com>
2026-01-31 15:45:02 +0100

    feat: add mouse follower

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
 import './css/main.scss';
 import './scripts/barba.js';
 import './scripts/smooth-scroll.js';
+import './scripts/mouse-follower.js';
---
+++ src/scripts/mouse-follower.js
@@ -0,0 +1,6 @@
+import MouseFollower from "mouse-follower";
+import gsap from "gsap";
+
+MouseFollower.registerGSAP(gsap);
+
+const cursor = new MouseFollower();
